da60aeb8b06c08149f034a65e44ff96a2b96194e
coshi190 <copasstragoon@gmail.com>
2025-04-10 11:09:39 +0700

    reorder nav menu

--- app/components/Headbar.tsx
+++ app/components/Headbar.tsx
@@ -12,8 +12,8 @@ export default function Headbar() {
                 <div className='flex flex-row items-center justify-between'>
                     <div className="gap-2 flex flex-row items-center p-6">
                         <Link href="/"><Button variant="ghost" className='cursor-pointer' size="icon"><img alt="" src="/favicon.ico" height="25" width="25" /></Button></Link>
-                        <Link href="/fields" className="text-white/70 hover:text-[#32ffa7] transition-colors text-sm font-mono"><Button variant="ghost" className='cursor-pointer'><span className="text-[#32ffa7]">&gt;</span> Fields</Button></Link>
-                        <Link href="/swap" className="text-white/70 hover:text-[#32ffa7] transition-colors text-sm font-mono"><Button variant="ghost" className='cursor-pointer'><span className="text-[#32ffa7]">&gt;</span> Swap</Button></Link>
+                        <Link href="/swap" className="text-white/70 hover:text-[#32ffa7] transition-colors text-sm font-mono"><Button variant="ghost" className='cursor-pointer'>Swap</Button></Link>
+                        <Link href="/fields" className="text-white/70 hover:text-[#32ffa7] transition-colors text-sm font-mono"><Button variant="ghost" className='cursor-pointer'>Fields</Button></Link>
                     </div>
                     <div className="flex align-center justify-end xl:mr-2">
                         {/* @ts-expect-error msg */}
