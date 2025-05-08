f94bdfe629fe9da2c71b6c83eb69a545d0df9120
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-05-08 20:08:06 +0700

    add CMswapUniSmartRouteContractV2 / UniswapPairv2PoolABI

--- app/lib/96.ts
+++ app/lib/96.ts
@@ -1,6 +1,6 @@
 import { createPublicClient, http, erc20Abi } from 'viem'
 import { bitkub } from 'viem/chains'
-import { NonfungiblePositionManager, v3Factory, v3Pool, qouterV2, router02, v3staker, WrappedNative, kap20abi } from '@/app/lib/abi'
+import { NonfungiblePositionManager, v3Factory, v3Pool, qouterV2, router02, v3staker, WrappedNative, kap20abi,CMswapUniSmartRouteABIV2,UniswapPair } from '@/app/lib/abi'
 
 // swap
 export const tokens: {name: string, value: '0xstring', logo: string}[] = [
@@ -14,6 +14,7 @@ export const POSITION_MANAGER = '0xb6b76870549893c6b59E7e979F254d0F9Cca4Cc9' as 
 export const positionManagerCreatedAt = BigInt(25033368)
 export const QOUTER_V2 = '0xCB0c6E78519f6B4c1b9623e602E831dEf0f5ff7f' as '0xstring'
 export const ROUTER02 = '0x3F7582E36843FF79F173c7DC19f517832496f2D8' as '0xstring'
+export const CMswapUniSmartRoute = '0x01837156518e60362048e78d025a419C51346f55' as '0xstring'
 export const v3FactoryContract = { chainId: 96, abi: v3Factory, address: V3_FACTORY } as const
 export const positionManagerContract = { chainId: 96, address: POSITION_MANAGER, abi: NonfungiblePositionManager } as const
 export const qouterV2Contract = { chainId: 96, abi: qouterV2, address: QOUTER_V2 } as const
@@ -24,5 +25,9 @@ export const v3PoolABI = { chainId: 96, abi: v3Pool } as const
 export const V3_STAKER = '0xC216ad61623617Aa01b757A06836AA8D6fb547fF' as '0xstring'
 export const v3StakerContract = { chainId: 96, address: V3_STAKER, abi: v3staker } as const
 export const wrappedNative = { chainId: 96, abi: WrappedNative, address: tokens[0].value } as const
+export const CMswapUniSmartRouteContractV2 =  { chainId: 96, abi: CMswapUniSmartRouteABIV2 , address: CMswapUniSmartRoute} as const
+export const UniswapPairv2PoolABI = { chainId: 96, abi: UniswapPair} as const
+
+
 
 export const publicClient = createPublicClient({ chain: bitkub, transport: http() })
