bc10a7603f22bc96f46b53646be83976a89f690a
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-05-12 21:57:58 +0700

    Update page.tsx

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -2,16 +2,11 @@
 import React from 'react'
 import { useAccount } from 'wagmi'
 import { type WriteContractErrorType } from '@wagmi/core'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { Card } from "@/components/ui/card"
 import ErrorModal from '@/app/components/error-modal'
-import Swap8899 from '../components/Swap8899'
-import Swap96 from '../components/Swap96'
-import Liquidity8899 from '../components/Liquidity8899'
-import Liquidity96 from '../components/Liquidity96'
-import Positions8899 from '../components/Positions8899'
-import Positions96 from '../components/Positions96'
+
 import Market96 from '../components/Market96'
+
+
 export default function Page() {
     const [isLoading, setIsLoading] = React.useState(false)
     const [errMsg, setErrMsg] = React.useState<WriteContractErrorType | null>(null)
@@ -22,7 +17,7 @@ export default function Page() {
             {isLoading && <div className="w-full h-full fixed backdrop-blur-[12px] z-999" />}
             <ErrorModal errorMsg={errMsg} setErrMsg={setErrMsg} />
                 <div className="px-4">
-                   <Market96 setIsLoading={setIsLoading} setErrMsg={setErrMsg}/>
+                   <Market96 />
                 </div>
         </div>
     )
