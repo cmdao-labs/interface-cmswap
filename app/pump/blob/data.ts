3cebff389b32852869def133bd923554c59f0699
coshi190 <copasstragoon@gmail.com>
2025-05-14 15:45:04 +0700

    feat: enable leaderboard

--- app/pump/blob/data.ts
+++ app/pump/blob/data.ts
@@ -1,10 +1,10 @@
 export const dataxp = [
     {
         addr: "0x1622a15BdA46B3b40c70a4688D3E8d1088d9C3bF".toUpperCase(),
-        xp: (500 * 13),
+        xp: (1),
     },
     {
         addr: "0x29cffc95d8B24B8ea110807617045eD484a05A46".toUpperCase(),
-        xp: (500 * 2),
+        xp: (1),
     }
 ] as const;
--- app/pump/launchpad/leaderboard/page.tsx
+++ app/pump/launchpad/leaderboard/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Suspense } from 'react';
 import type { Metadata } from "next";
-// import Leaderboard from "@/app/pump/ui/Leaderboard";
+import Leaderboard from "@/app/pump/ui/Leaderboard";
 import Sort2 from "@/app/pump/ui/Sort2";
 
 export const metadata: Metadata = {
@@ -23,9 +23,9 @@ export default async function LeaderboardPage(props: {
   const chain = searchParams?.chain || '';
 
   return (
-    <main className="row-start-2 w-full sm:w-1/2 self-center h-full flex flex-col gap-6 items-center sm:items-start">
+    <main className="row-start-2 w-full sm:w-1/2 self-center h-full flex flex-col gap-6 items-center sm:items-start mt-[150px]">
       <Link href={"/pump/launchpad?chain=" + chain + (mode === 'pro' ? "&mode=pro" : "&mode=lite")} prefetch={false} className="underline hover:font-bold">Back to launchpad</Link>
-      <span className="text-2xl font-bold my-2">Quest</span>
+      {/* <span className="text-2xl font-bold my-2">Quest</span>
       <Link href="https://forms.gle/MxQfSDHpyx5tDFvQA" rel="noopener noreferrer" target="_blank" className="w-[300px] h-[420px] rounded-2xl shadow-2xl bg-slate-300 bg-opacity-25 flex flex-col items-center align-center overflow-hidden hover:cursor-pointer hover:shadow-emerald-300">
         <div className="w-[300px] h-[300px] overflow-hidden flex flex-wrap content-center justify-center">
           <div className="w-[300px] h-[300px] relative">
@@ -37,7 +37,7 @@ export default async function LeaderboardPage(props: {
           <span className="text-lg font-bold">Everyday Social Contributor</span>
           <span className="text-sm text-right">🎁 500 XP</span>
         </div>
-      </Link>
+      </Link> */}
       <span className="text-2xl font-bold mt-4 mb-2">Leaderboard 🏆</span>
       <Sort2 />
       <Suspense key={rankby + mode} fallback={
@@ -49,7 +49,7 @@ export default async function LeaderboardPage(props: {
           <div className="w-full h-[70px] sm:px-14 sm:py-10 bg-gray-500 rounded-lg mb-1 animate-pulse" />
         </div>
       }>
-        {/* <Leaderboard rankby={rankby} mode={mode} chain={chain} /> */}
+        <Leaderboard rankby={rankby} mode={mode} chain={chain} />
       </Suspense>
     </main>
   );
