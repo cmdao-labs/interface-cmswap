3386b79d7eb863a52932a7175b1339b863ac3a53
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-05-29 20:22:35 +0700

    minimize height for geckoterminal

--- app/pump/ui/Trade.tsx
+++ app/pump/ui/Trade.tsx
@@ -973,9 +973,9 @@ React.useEffect(() => {
                  {
                     grapthType === "Geckoterminal" 
                     ?
-                    <iframe height="35%" width="100%" id="geckoterminal-embed" title="GeckoTerminal Embed" src={"https://www.geckoterminal.com/" + (chain === "kub" ? "bitkub_chain" : chain === "monad" ? "monad-testnet" : '') + "/pools/" + lp + "?embed=1&info=0&swaps=0&grayscale=0&light_chart=0&chart_type=market_cap&resolution=1m"} allow="clipboard-write"></iframe>
+                    <iframe height="28%" width="100%" id="geckoterminal-embed" title="GeckoTerminal Embed" src={"https://www.geckoterminal.com/" + (chain === "kub" ? "bitkub_chain" : chain === "monad" ? "monad-testnet" : '') + "/pools/" + lp + "?embed=1&info=0&swaps=0&grayscale=0&light_chart=0&chart_type=market_cap&resolution=1m"} allow="clipboard-write"></iframe>
                     :
-                    <Chart />   
+                        <Chart />   
                 }
 
               
