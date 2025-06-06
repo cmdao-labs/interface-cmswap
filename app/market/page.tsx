9ff5022578897f927aed2acb9165063581569a89
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-06-06 14:19:45 +0700

    feat : get ref params

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -5,6 +5,7 @@ import { type WriteContractErrorType } from '@wagmi/core'
 import ErrorModal from '@/app/components/error-modal'
 
 import Market96 from '../components/Market96'
+import ReferralTracker from '../components/Refferal'
 
 
 export default function Page() {
@@ -14,10 +15,11 @@ export default function Page() {
 
     return (
         <div className="min-h-screen w-full flex flex-col items-center justify-start text-xs bg-[#0a0b1e] bg-[linear-gradient(to_bottom,rgba(0,0,0,0.8),rgba(0,0,0,0.5))]">
+            <ReferralTracker/>
             {isLoading && <div className="w-full h-full fixed backdrop-blur-[12px] z-999" />}
             <ErrorModal errorMsg={errMsg} setErrMsg={setErrMsg} />
                 <div className="px-4">
-                   <Market96 />
+                   {chainId === 96 || chainId === undefined && <Market96 />}
                 </div>
         </div>
     )
