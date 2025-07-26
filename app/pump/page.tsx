6b6d9d3457bea136ee3cb53f1b9cdb1323ee382f
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-07-26 19:54:09 +0700

    feat : redirect default with current chainId

--- app/pump/page.tsx
+++ app/pump/page.tsx
@@ -1,5 +1,14 @@
+'use client';
 import { redirect } from 'next/navigation';
+import { useAccount } from "wagmi";
 
 export default function Home() {
-  redirect('/pump/launchpad?chain=kub&mode=pro');
+  const { chainId } = useAccount()
+  if(chainId === 96){
+    redirect('/pump/launchpad?chain=kub&mode=pro');
+  }else if(chainId === 10143){
+    redirect('/pump/launchpad?chain=monad&mode=pro');
+  }else if(chainId === 25925){
+    redirect('/pump/launchpad?chain=kubtestnet&mode=pro');
+  }
 }
