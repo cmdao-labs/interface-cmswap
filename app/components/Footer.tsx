10f7b03a0b4c081322fc291d05de12cd84581142
coshi190 <copasstragoon@gmail.com>
2025-08-18 10:54:30 +0700

    0.1.2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link"
-import { Github, FileText, MessageSquare, X,Scale,Percent,ShieldCheck } from "lucide-react"
-import {FaTwitter,FaUsers,FaTelegram } from "react-icons/fa";
-import { BsTwitterX,BsDiscord  } from "react-icons/bs";
-import { MdContacts } from "react-icons/md";
-import { LuChartCandlestick } from "react-icons/lu";
+import { Github, FileText } from "lucide-react"
+import { FaTelegram } from "react-icons/fa"
+import { BsTwitterX, BsDiscord  } from "react-icons/bs"
+import getConfig from 'next/config'
 
 export default function Footer() {
+    const { publicRuntimeConfig } = getConfig()
+
     return (
         <footer className="relative z-10 bg-black/80">
             <div className="container mx-auto px-4 py-6">
                 <div className="flex flex-col md:flex-row justify-between items-start ml-[20px] md:ml-[0px] md:items-center gap-4">
                     <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
-                        <span className="text-xs font-mono">CMswap_v0.1.1</span>
+                        <span className="text-xs font-mono">v{process.env.APP_VERSION}</span>
                         <div className="h-4 w-px bg-[#32ffa7]/20 hidden md:block" />
                         <Link href="https://docs.cmswap.xyz/" target="_blank" rel="noreferrer" className="text-white/60 hover:text-[#32ffa7] text-xs flex items-center gap-1  font-mono">
                             <FileText className="w-4 h-4" /><span>DOCS</span>
