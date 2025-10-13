1e5b9c4fd668fa0e58254d46bdc08867a6ee4867
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2025-10-13 12:51:28 +0700

    feat : automated route to v1 for chain Kub & Monad Testnet

--- app/pump/page.tsx
+++ app/pump/page.tsx
@@ -4,5 +4,7 @@ import { useAccount } from "wagmi";
 
 export default function Home() {
     const { chainId } = useAccount();
-    if (chainId === 25925 || chainId === null || chainId === undefined) redirect('/pump/launchpad?chain=kubtestnet&mode=pro');
+    if (chainId === 25925 || chainId === null || chainId === undefined) redirect('/pump/launchpad?chain=kubtestnet&mode=pro')
+    else if(chainId === 96) redirect('https://www.cmswap.fun/pump/launchpad?chain=kub&mode=pro');
+    else if(chainId === 10143) redirect('https://www.cmswap.fun/pump/launchpad?chain=monad&mode=pro');
 }
